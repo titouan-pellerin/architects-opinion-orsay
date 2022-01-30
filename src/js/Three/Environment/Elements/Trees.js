c3d70084ece3adca97c3852458725912af945b0e
Titouan Pellerin <titouan.pellerin@gmail.com>
2022-01-30 22:27:56 +0100

    wip opti

--- src/js/Three/Environment/Elements/Trees.js
+++ src/js/Three/Environment/Elements/Trees.js
@@ -76,7 +76,8 @@ export class Trees extends THREE.Group {
 
     let noise2D;
 
-    const instanceNumber = 1000;
+    // const instanceNumber = 1000;
+    const instanceNumber = 700;
     const instance = new THREE.Object3D();
 
     this.geometry = new THREE.PlaneGeometry(0.3, 0.3, 1, 4);
