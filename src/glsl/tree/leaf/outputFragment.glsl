e372330dd202cebe5e5d0e967e27e29eade82317
Titouan Pellerin <titouan.pellerin@gmail.com>
2022-02-21 11:55:47 +0100

    trees leaves colors raycaster better

--- src/glsl/tree/leaf/outputFragment.glsl
+++ src/glsl/tree/leaf/outputFragment.glsl
@@ -3,7 +3,7 @@
 // Shape
 float rightPattern = 1.0 - (step(1.0, length(vUv)));
 float leftPattern = 1.0 - (step(1.0, length(1.0 - vUv)));
-vec4 shape = vec4(rightPattern) * vec4(leftPattern) * vec4(uColor, 1.0);
+vec4 shape = vec4(rightPattern) * vec4(leftPattern) * vec4(mix(uColor, vec3(3.), vRayModifier), 1.);
 
 // Noise
 float noise = cnoise(vUv * 10.);
@@ -40,7 +40,7 @@ vec4 lines = (centerLineFinal + rightLineFinal + leftLineFinal) * vec4(uColor2, 
 
 // Global render
 vec4 render = (shape + lines * (1.0 - noiseRender)) * vec4(vec3(fade), 1.0);
-render.rgb = mix(render.rgb, vec3(3.), vRayModifier);
+// render.rgb = mix(render.rgb, vec3(3.), vRayModifier);
 gl_FragColor = vec4(outgoingLight * render.rgb, diffuseColor.a * render.a);
 // gl_FragColor = vec4(render.rgb, render.a);
 
