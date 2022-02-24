5d32bdc53777a5736824b328a69225539ee3da93
Titouan Pellerin <titouan.pellerin@gmail.com>
2022-02-24 14:45:54 +0100

    river raycaster wip

--- src/glsl/ground/river/beginVertex.glsl
+++ src/glsl/ground/river/beginVertex.glsl
@@ -16,11 +16,13 @@ vNoise2 = noise2;
 vNoise3 = noise3;
 vNoise4 = noise4;
 
-float rayModifier = mix(.001, 0., clamp((distance(rayPos, vec3(worldPosition.x, - 2.5, worldPosition.z))), .0, 1.) * 1.);
+float distanceMax = 1. + cnoise(vec2(transformed.z * 50., uTime));
 
-vRayModifier = rayModifier * 1000.;
+float rayModifier = mix(.002, 0., clamp((distance(rayPos, vec3(worldPosition.x, - 2.5, worldPosition.z))), .0, distanceMax) / distanceMax);
 
-transformed.z -= rayModifier;
+vRayModifier = rayModifier * 500.;
+
+transformed.z += rayModifier;
 
 transformed.z += noise;
 transformed.z += noise2;
--- src/glsl/ground/river/outputFragment.glsl
+++ src/glsl/ground/river/outputFragment.glsl
@@ -4,7 +4,7 @@ vec3 render3 = uColor + vec3(vNoise3) * 2000.;
 vec3 render4 = uColor2 + vec3(vNoise4) * 2000.;
 
 vec3 renderMix = (render * render2) + (render3 * render4);
-renderMix = mix(renderMix, uColor2, vRayModifier);
+renderMix = mix(renderMix, uColor2 * 2., vRayModifier);
 
 // gl_FragColor = vec4(renderMix, renderMix.g);
 gl_FragColor = vec4(outgoingLight * renderMix, diffuseColor.a);
