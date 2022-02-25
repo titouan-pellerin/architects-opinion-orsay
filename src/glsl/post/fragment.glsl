4f34100055e2803218c8d15bb3e7228baa663e7b
Thoma Lecornu <thoma.lecornu@gmail.com>
2022-02-25 14:14:14 +0100

    dsfdfds

--- src/glsl/post/fragment.glsl
+++ src/glsl/post/fragment.glsl
@@ -2,16 +2,9 @@
 
 #define PI 3.1415926535897932384626433832795
 
-varying vec2 vUv;
-
 uniform sampler2D tDiffuse;
-uniform sampler2D uNoiseTexture;
+varying vec2 vUv;
 uniform float uTime;
-uniform float uProgress;
-uniform float uFadeProgress;
-uniform float uBorderFadeProgress;
-uniform float uSunProgress;
-uniform float uMenuSwitch;
 uniform float uCornerIntensity;
 uniform float uCornerSize;
 uniform vec2 uRes;
@@ -31,28 +24,21 @@ vec2 rotate(vec2 uv, float rotation, vec2 mid) {
   return vec2(cos(rotation) * (uv.x - mid.x) + sin(rotation) * (uv.y - mid.y) + mid.x, cos(rotation) * (uv.y - mid.y) - sin(rotation) * (uv.x - mid.x) + mid.y);
 }
 
-float parabola(float x) {
-  return -2.8 * pow(x, 2.) + 2.8 * x + 0.3;
-}
-
 void main() {
+
+  vec2 rotateTest = rotate(uBlurPos, sin(uTime) + PI * 0.5, vec2(0., 0.)); 
 
     // Tint
   vec4 TintColor = vec4(uTintColor, 1.0);
-
-  vec2 rotateTest = rotate(uBlurPos, sin(uTime) + PI * 0.5, vec2(0., 0.));
-  float noise = 1.0 - abs(cnoise(vUv * 8. + uTime * 0.25));
-
-  float transitionNoise = 1.0 - cnoise(6. * vUv + (uTime * 0.5));
 
     // Corner
   float corner = pow(1.0 - distance(vUv, vec2(0.5)), uCornerSize);
   vec4 cornerColor = vec4(corner + uCornerIntensity) + vec4(uCornerColor, 1.0);
 
-  // float fakeligthing = 1.0 - cfakeligthing(vUv * 20.);
+  float noise = 1.0 - abs(cnoise(vUv * 7. + uTime * 0.25));
+  // float noise = 1.0 - cnoise(vUv * 20.);
     // Part1, tint & corner
   // vec4 p1 = texture2D(tDiffuse, vUv) * cornerColor * TintColor * 0.5;
-
   vec4 p1 = texture2D(tDiffuse, vUv) * 0.5 * cornerColor;
 
     // Blur
@@ -61,9 +47,7 @@ void main() {
   float total = 0.0;
 
   // vec2 toCenter = uBlurPos * abs(sin(uTime)) - vUv * uRes;
-  // vec2 toCenter = uBlurPos * parabola(uSunProgress) - vUv * uRes;
-  vec2 toCenter = vec2(uSunProgress, parabola(uSunProgress)) * uBlurPos - vUv * uRes;
-  // vec2 toCenter = uBlurPos - vUv * uRes;
+  vec2 toCenter = uBlurPos - vUv * uRes;
 
   float offset = random(vec3(12.9898, 78.233, 151.7182), 0.0);
 
@@ -81,12 +65,14 @@ void main() {
     total += weight;
   } 
 
-  // Part2, adding some blur
+    // Part2, adding some blur
+  // vec4 p2 = ((color / total)) * 0.75;
   vec4 p2 = ((color / total));
 
   vec2 texel = vec2(1. / uRes.x, 1. / uRes.y);
 
-	// kernel definition (in glsl matrices are filled in column-major order)
+		// kernel definition (in glsl matrices are filled in column-major order)
+
   const mat3 Gx = mat3(-1, -2, -1, 0, 0, 0, 1, 2, 1); // x direction kernel
   const mat3 Gy = mat3(-1, 0, 1, -2, 0, 2, -1, 0, 1); // y direction kernel
 
@@ -118,33 +104,10 @@ void main() {
     Gy[0][2] * tx0y2 + Gy[1][2] * tx1y2 + Gy[2][2] * tx2y2;
 
 		// magnitute of the total gradient
-  float G = pow(abs(noise * 2.0 + 0.35), sqrt((valueGx * valueGx * noise) + (valueGy * valueGy * noise)));
-  float G2 = pow(2.0, sqrt((valueGx * valueGx) + (valueGy * valueGy)));
+  float G = pow(abs(noise + 0.75), sqrt((valueGx * valueGx * noise) + (valueGy * valueGy * noise)));
 
-  vec4 mainRender = (p1 + p2) * vec4(G);
-  // vec4 menuRender = ((p2 * 0.75) * vec4(G2));
-  vec4 menuRender = ((p2 * 0.5) * vec4(G2 * uBorderFadeProgress)); // smooth color à changer
-
-  float noiseTexture = texture2D(uNoiseTexture, 0.5 * (vUv + 1.0)).r;
-
-  float temp = uProgress;
-  temp += ((10.0 * noiseTexture - 5.0 * transitionNoise) * 0.05) - 0.3;
-  temp *= G * 0.9;
-
-  float distanceFromCenter = length(vUv - 0.5);
-  temp = smoothstep(temp - 0.05, temp, distanceFromCenter);
-
-  vec4 finalRender;
-
-  if(uMenuSwitch == 0.) {
-    finalRender = mix(menuRender, mainRender, temp);
-  }
-  if(uMenuSwitch == 1.) {
-    finalRender = mix(mainRender, menuRender, temp);
-  }
-  if(uMenuSwitch == 2.) {
-    finalRender = mix(menuRender, mainRender, uFadeProgress);
-  }
-
-  gl_FragColor = finalRender;
+    // gl_FragColor = render;
+  gl_FragColor = texture2D(tDiffuse, vUv);
+  gl_FragColor = p2;
+  gl_FragColor = (p1 + p2) * vec4(G);
 }
